bd25e27eda2edf2af4ddeacbd5b2c0377bff9ca1
Yaroslav Shkvarla <slav.shkvarlo@gmail.com>
2023-01-05 13:59:24 +0200

    second commit

---
+++ src/main/java/com/example/cryptoparser/CryptoparserApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptoparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoparserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoparserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cryptoparser/CryptoparserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptoparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoparserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
